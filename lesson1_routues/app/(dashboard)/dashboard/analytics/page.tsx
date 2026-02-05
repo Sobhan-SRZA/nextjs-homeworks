c6fee06863e4f61c2314d49c1c6726ed3de5487e
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-02-05 13:43:09 +0330

    Create page.tsx

---
+++ lesson1_routues/app/(dashboard)/dashboard/analytics/page.tsx
@@ -0,0 +1,11 @@
+function Analytics() {
+    return (
+        <>
+            <h1 className="text-2xl">
+                Dashboard Analytics page
+            </h1>
+        </>
+    )
+}
+
+export default Analytics
