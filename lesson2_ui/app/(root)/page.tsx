ccef09b6777c9c30f00129a2562a6199a6453a0e
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-02-05 22:53:42 +0330

    Create page.tsx

---
+++ lesson2_ui/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <h1 className="text-2xl">
+        Next.JS Practice
+      </h1>
+      <p className="mt-3">
+        Welcome to this project.
+      </p>
+    </>
+  )
+}
+
+export default Home
