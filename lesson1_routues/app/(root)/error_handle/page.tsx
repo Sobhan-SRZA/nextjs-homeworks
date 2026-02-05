bee9e8ed245ee4d065679550c13ed100fcbe7823
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-02-05 13:43:22 +0330

    Create page.tsx

---
+++ lesson1_routues/app/(root)/error_handle/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+throw new Error("this page is in the development!")
+
+const ErrorHandle = () => {
+    return (
+        <div>
+            ErrorHandle test
+        </div>
+    )
+}
+
+export default ErrorHandle
