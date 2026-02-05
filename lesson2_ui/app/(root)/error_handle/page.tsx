855d67efad1003616af6c2618e7a04916b0bc09d
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-02-05 22:53:37 +0330

    Create page.tsx

---
+++ lesson2_ui/app/(root)/error_handle/page.tsx
@@ -0,0 +1,10 @@
+const ErrorHandle = () => {
+    throw new Error("this page is in the development!")
+    return (
+        <div>
+            ErrorHandle test
+        </div>
+    )
+}
+
+export default ErrorHandle
