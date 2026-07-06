f2956ab48184be4aa24e62e5cf7fc3ad532c34da
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-06 15:45:55 +0330

    Update page.tsx

--- lesson2_ui/app/(root)/page.tsx
+++ lesson2_ui/app/(root)/page.tsx
@@ -10,9 +10,23 @@ function Home() {
         Welcome to this project.
       </p>
 
-      <Link href={"/about"}>
-      Go to About page
-      </Link>
+      <div className="relative my-2 w-max place-self-center hover:text-blue-400 hover:bg-blue-50 transition cursor-pointer text-blue-200 border-2 rounded-2xl p-2 bg-blue-500/50 backdrop-blur-sm">
+        <Link href={"/about"}>
+          Go to About page
+        </Link>
+      </div>
+
+      <div className="relative my-2 w-max place-self-center hover:text-blue-400 hover:bg-blue-50 transition cursor-pointer text-blue-200 border-2 rounded-2xl p-2 bg-blue-500/50 backdrop-blur-sm">
+        <Link href={"/dashboard/analytics"}>
+          Got to users analytics
+        </Link>
+      </div>
+
+      <div className="relative my-2 w-max place-self-center hover:text-blue-400 hover:bg-blue-50 transition cursor-pointer text-blue-200 border-2 rounded-2xl p-2 bg-blue-500/50 backdrop-blur-sm">
+        <Link href={"/dashboard/users"}>
+          Go to Users dashboard
+        </Link>
+      </div>
     </>
   )
 }
