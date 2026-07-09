04dc3407665958b26f3f453bcf59b931dc6d82fe
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-09 04:45:34 +0330

    Update page.tsx

--- lesson3_jobtracker/app/dashboard/page.tsx
+++ lesson3_jobtracker/app/dashboard/page.tsx
@@ -1,4 +1,28 @@
-export default function Dashboard() {
+import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import { redirect } from "next/navigation";
+
+async function LoadDashboard(params: any) {
+
+}
+
+export default async function Dashboard() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/sign-in")
+  }
+
+  await connectDB();
+
+  const baord = await Board.findOne({
+    userId: session.user.id,
+    name: "Job Hunt"
+  });
+  console.log("🚀 ~ Dashboard ~ baord:", baord)
+
+
   return (
     <div>Dashboard page</div>
   )
