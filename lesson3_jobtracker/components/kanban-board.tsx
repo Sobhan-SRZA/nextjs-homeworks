d56c3487bf70276e62221b410c316604d039079b
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-09 05:08:19 +0330

    Create kanban-board.tsx

---
+++ lesson3_jobtracker/components/kanban-board.tsx
@@ -0,0 +1,98 @@
+"use clinet";
+
+import {
+  Award,
+  Calendar,
+  CheckCircle2,
+  Mic,
+  XCircle
+} from "lucide-react";
+import { Board, Column } from "@/lib/models/models.types";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+
+interface KanbanBoardProbs {
+  board: Board;
+  userId: string;
+}
+
+interface ColumnConfig {
+  color: string;
+  icon: React.ReactNode;
+}
+
+const COLUMN_CONFIG: Array<ColumnConfig> = [
+  {
+    color: "bg-cyan-500",
+    icon: <Calendar className="h-4 w-4" />
+  },
+  {
+    color: "bg-purple-500",
+    icon: <CheckCircle2 className="h-4 w-4" />
+  },
+  {
+    color: "bg-green-500",
+    icon: <Mic className="h-4 w-4" />
+  },
+  {
+    color: "bg-yellow-500",
+    icon: <Award className="h-4 w-4" />
+  },
+  {
+    color: "bg-red-500",
+    icon: <XCircle className="h-4 w-4" />
+  }
+];
+
+interface DroppableColumnProbs {
+  column: Column;
+  config: ColumnConfig;
+  boardId: string;
+}
+
+function DroppableColumn({ column, config, boardId }: DroppableColumnProbs) {
+  return (
+    <Card>
+      <CardHeader className={`${config.color}`}>
+        <div>
+          <div>
+            {config.icon}
+            <CardTitle>
+              {column.name}
+            </CardTitle>
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  )
+}
+
+
+export default function KanbanBoard({ board, userId }: KanbanBoardProbs) {
+  const columns = board.columns;
+
+  return (
+    <>
+      <div>
+        <div>
+          {
+            columns.map((column, index) => {
+              const config = COLUMN_CONFIG[index] || {
+                color: "bg-gray-500",
+                icon: <Calendar className="h-4 w-4" />
+              };
+
+              return (
+                <DroppableColumn
+                  key={index}
+                  column={column}
+                  config={config}
+                  boardId={board._id}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
+}
