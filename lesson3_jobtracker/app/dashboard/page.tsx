e8b863f22d50ea822e24098cf236a085bddb27b2
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-10 13:03:24 +0330

    Update page.tsx

--- lesson3_jobtracker/app/dashboard/page.tsx
+++ lesson3_jobtracker/app/dashboard/page.tsx
@@ -1,45 +1,61 @@
 import { getSession } from "@/lib/auth/auth";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 import { Board } from "@/lib/models";
 import KanbanBoard from "@/components/kanban-board";
 import connectDB from "@/lib/db";
 
-async function LoadDashboard(params: any) {
-
-}
-
-export default async function Dashboard() {
-  const session = await getSession();
-
-  if (!session?.user) {
-    redirect("/sign-in")
-  }
+async function getBoard(userId: string) {
+  "use cache";
 
   await connectDB();
 
-  const board = await Board.findOne({
-    userId: session.user.id,
+  const boardDoc = await Board.findOne({
+    userId: userId,
     name: "Job Hunt"
   })
 
     .populate({
-      path: "columns"
+      path: "columns",
+      populate: {
+        path: "jobApplications"
+      }
     });
 
+  if (!boardDoc)
+    return null;
+
+  const board = JSON.parse(JSON.stringify(boardDoc));
+
+  return board;
+}
+
+async function DashboardPage() {
+  const session = await getSession();
+  const board = await getBoard(session?.user.id ?? "");
+
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
 
   return (
     <div className="min-h-screen bg-white">
       <div className="container mx-auto p-6">
         <div className="mb-6">
-          <h1 className="text-3xl font-bold text-black">{board.name}</h1>
+          <h1 className="text-3xl font-bold text-black">Job Hunt</h1>
           <p className="text-gray-600">Track your job applications</p>
         </div>
-
-        <KanbanBoard
-          board={JSON.parse(JSON.stringify(board))}
-          userId={session.user.id}
-        />
+        <KanbanBoard board={board} userId={session.user.id} />
       </div>
     </div>
-  )
+  );
+}
+
+
+export default async function Dashboard() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <DashboardPage />
+    </Suspense>
+  );
 }
