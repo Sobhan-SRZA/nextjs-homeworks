c4d570860f6e169523ed735a6286bfd82c2b5487
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-15 06:43:51 +0330

    Update page.tsx

--- lesson4_devevent/app/page.tsx
+++ lesson4_devevent/app/page.tsx
@@ -1,4 +1,16 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+
+const events = [
+  {
+    image: "/images/event1.png",
+    title: "Event 1"
+  },
+  {
+    image: "/images/event2.png",
+    title: "Event 2"
+  }
+]
 
 export default function Home() {
   return (
@@ -7,6 +19,21 @@ export default function Home() {
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
       <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {
+            events
+              .map((event) => (
+                <li key={event.title}>
+                  <EventCard {...event} />
+                </li>
+              ))
+          }
+        </ul>
+      </div>
     </section>
   );
 }
