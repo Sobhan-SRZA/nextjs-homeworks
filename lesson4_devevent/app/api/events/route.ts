fe407c4f700b4189af8aa36d27f3855e19fb3197
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-16 07:33:29 +0330

    Update route.ts

--- lesson4_devevent/app/api/events/route.ts
+++ lesson4_devevent/app/api/events/route.ts
@@ -1,4 +1,4 @@
-import {NextRequest, NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { v2 as cloudinary } from 'cloudinary';
 
 import connectDB from "@/lib/mongodb";
@@ -15,15 +15,15 @@ export async function POST(req: NextRequest) {
         try {
             event = Object.fromEntries(formData.entries());
         }
-        
+
         catch (e) {
-            return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400 })
+            return NextResponse.json({ message: 'Invalid JSON data format' }, { status: 400 })
         }
 
         const file = formData.get('image') as File;
 
-        if(!file)
-            return NextResponse.json({ message: 'Image file is required'}, { status: 400 })
+        if (!file)
+            return NextResponse.json({ message: 'Image file is required' }, { status: 400 })
 
         let tags = JSON.parse(formData.get('tags') as string);
         let agenda = JSON.parse(formData.get('agenda') as string);
@@ -32,11 +32,14 @@ export async function POST(req: NextRequest) {
         const buffer = Buffer.from(arrayBuffer);
 
         const uploadResult = await new Promise((resolve, reject) => {
-            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvent' }, (error, results) => {
-                if(error) return reject(error);
+            cloudinary.uploader
+                .upload_stream({ resource_type: 'image', folder: 'DevEvent' }, (error, results) => {
+                    if (error)
+                        return reject(error);
 
-                resolve(results);
-            }).end(buffer);
+                    resolve(results);
+                })
+                .end(buffer);
         });
 
         event.image = (uploadResult as { secure_url: string }).secure_url;
@@ -48,12 +51,12 @@ export async function POST(req: NextRequest) {
         });
 
         return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
-    } 
-    
+    }
+
     catch (e) {
         console.error(e);
 
-        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 500 })
+        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown' }, { status: 500 })
     }
 }
 
@@ -64,8 +67,8 @@ export async function GET() {
         const events = await Event.find().sort({ createdAt: -1 });
 
         return NextResponse.json({ message: 'Events fetched successfully', events }, { status: 200 });
-    } 
-    
+    }
+
     catch (e) {
         return NextResponse.json({ message: 'Event fetching failed', error: e }, { status: 500 });
     }
