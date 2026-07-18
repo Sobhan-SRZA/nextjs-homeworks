9813060a9cf3f427b2394f245959aa936ad61be0
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-18 06:32:14 +0330

    Update navbar.tsx

--- lesson5_onlineshop/components/navbar.tsx
+++ lesson5_onlineshop/components/navbar.tsx
@@ -1,10 +1,35 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Navbar() {
+    const navLinks = [
+        {
+            href: "/",
+            title: "Home"
+        },
+        {
+            href: "/store",
+            title: "Store"
+        }
+    ];
+
+    const pathName = usePathname();
+
     return (
-        <nav>
-            <Link href={"/"}>Home</Link>
-            <Link href={"/store"}>Store</Link>
+        <nav className="shadow p-4">
+            {
+                navLinks.map(
+                    link => (
+                        <Link
+                            key={link.href}
+                            className={`mr-4${pathName === link.href ? " text-sky-500" : ""}`}
+                            href={link.href}
+                        >{link.title}</Link>
+                    )
+                )
+            }
         </nav>
     )
 }
