c5d80f253fd5e71ce130de45adcf10d535993af8
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-18 06:41:49 +0330

    Update navbar.tsx

--- lesson5_onlineshop/components/navbar.tsx
+++ lesson5_onlineshop/components/navbar.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import Container from "./Container";
 
 export default function Navbar() {
     const navLinks = [
@@ -19,17 +20,19 @@ export default function Navbar() {
 
     return (
         <nav className="shadow p-4">
-            {
-                navLinks.map(
-                    link => (
-                        <Link
-                            key={link.href}
-                            className={`mr-4${pathName === link.href ? " text-sky-500" : ""}`}
-                            href={link.href}
-                        >{link.title}</Link>
+            <Container>
+                {
+                    navLinks.map(
+                        link => (
+                            <Link
+                                key={link.href}
+                                className={`mr-4${pathName === link.href ? " text-sky-500" : ""}`}
+                                href={link.href}
+                            >{link.title}</Link>
+                        )
                     )
-                )
-            }
+                }
+            </Container>
         </nav>
     )
 }
