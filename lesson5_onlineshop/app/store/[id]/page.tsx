cafc9404bb5c0df3f775453ae3bd63d307c9bd09
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-19 06:43:31 +0330

    Update page.tsx

--- lesson5_onlineshop/app/store/[id]/page.tsx
+++ lesson5_onlineshop/app/store/[id]/page.tsx
@@ -1,15 +1,24 @@
+import { ProductItemProbs } from "@/components/ProductItem";
 import Container from "@/components/Container";
 
-export default function Product() {
+interface ProductProbs { params: Promise<{ id: string; }> }
+
+export default async function Product({
+    params
+}: ProductProbs) {
+    const { id } = await params;
+    const result = await fetch(`http://localhost:3001/products/${id}`);
+    const product = await result.json() as ProductItemProbs;
+
     return (
         <Container>
             <div className="grid grid-cols-12 mt-8 shadow-md">
                 <div className="col-span-9 rtl text-right p-4">
-                    <h2 className="font-bold text-2xl">محصول اول</h2>
+                    <h2 className="font-bold text-2xl">{product.title}</h2>
 
-                    <p className="text-gray-300"> دزدسیدنتسیزدسسد ستنزد س ستند غع للغع عغ لغ علخع لعغ لخ غل  لغعخ لغل ع لخعه ل عهل عا لسعخهی س خسل سلخهع </p>
+                    <p className="text-gray-300">{product.description}</p>
 
-                    <p className="font-bold">قیمت <span>20$</span></p>
+                    <p className="font-bold">قیمت <span>{product.price}$</span></p>
 
                     <div className="mt-4">
                         <button className="px-4 py-2 rounded-2xl bg-sky-500">+</button>
@@ -19,7 +28,7 @@ export default function Product() {
                 </div>
 
                 <div className="col-span-3">
-                    <img src="/images/product 1.png" alt="product 1" />
+                    <img src={product.image} alt={product.title} />
                 </div>
             </div>
         </Container>
