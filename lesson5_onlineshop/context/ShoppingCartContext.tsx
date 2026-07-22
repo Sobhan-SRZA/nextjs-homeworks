ad98f468badd7093fdb5e57ca29890008be255cd
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-22 06:28:28 +0330

    Update ShoppingCartContext.tsx

--- lesson5_onlineshop/context/ShoppingCartContext.tsx
+++ lesson5_onlineshop/context/ShoppingCartContext.tsx
@@ -1,19 +1,27 @@
 "use client"
 
-import { createContext, useState } from "react"
+import { createContext, useContext, useState } from "react"
 
 type CartItem = {
     id: number;
     qty: number;
 }
 
-const ShoppingCartContext = createContext({});
+type ShoppingCartContextType = {
+    cartItems: CartItem[];
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContextType);
+
+export const useShoppingCartContext = () => {
+    return useContext(ShoppingCartContext);
+};
 
 export default function ShoppingCartContextProvider({ children }: { children: React.ReactNode; }) {
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
     return (
-        <ShoppingCartContext.Provider value={{}}>
+        <ShoppingCartContext.Provider value={{ cartItems }}>
             {children}
         </ShoppingCartContext.Provider>
     )
