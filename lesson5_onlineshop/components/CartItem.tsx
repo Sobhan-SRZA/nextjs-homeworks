208efd23b84351b3547cc55bf32f0a0633a38237
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-24 13:59:03 +0330

    Update CartItem.tsx

--- lesson5_onlineshop/components/CartItem.tsx
+++ lesson5_onlineshop/components/CartItem.tsx
@@ -2,6 +2,7 @@ import {
     useEffect,
     useState
 } from "react";
+import { formatNumberWithCommas } from "@/utils/number";
 import { ProductItemProbs } from "./ProductItem";
 import AddToCart from "./AddToCart";
 import axios from "axios";
@@ -20,16 +21,16 @@ export default function CartItem({ id, qty }: CartItemProbs) {
                 const { data } = result;
                 setData(data);
             });
-    }, [])
+    }, []);
 
     return (
-        <div className="grid grid-cols-12 bg-gray-700">
+        <div className="grid grid-cols-12 bg-gray-700 rounded">
             <div className="col-span-10 rtl p-4">
                 <h2 className="text-xl font-bold">{data.title}</h2>
 
                 <p>تعداد: <span>{qty}</span></p>
 
-                <p>قیمت محصول: <span>{data.price}$</span></p>
+                <p>قیمت محصول: <span>{formatNumberWithCommas(data.price ?? 0)}$</span></p>
 
                 <AddToCart id={id.toString()} />
             </div>
