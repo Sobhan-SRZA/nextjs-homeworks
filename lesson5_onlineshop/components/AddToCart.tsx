734fb05c7f9d1869c6c5039985f2d1a337dbb719
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-24 08:30:19 +0330

    Update AddToCart.tsx

--- lesson5_onlineshop/components/AddToCart.tsx
+++ lesson5_onlineshop/components/AddToCart.tsx
@@ -12,16 +12,22 @@ export default function AddToCart({
     const { handleIncreaseProductQty, handleDecreaseProductQty, getProductQty } = useShoppingCartContext();
 
     return (
-        <div className="mt-4">
-            <button
-                className="px-4 py-2 rounded-2xl bg-sky-500"
-                onClick={() => handleIncreaseProductQty(parseInt(id))}
-            >+</button>
-            <span className="mx-4">{getProductQty(parseInt(id))}</span>
-            <button
-                className="px-4 py-2 rounded-2xl bg-sky-500"
-                onClick={() => handleDecreaseProductQty(parseInt(id))}
-            >-</button>
-        </div >
+        <div>
+            <div className="mt-4">
+                <button
+                    className="px-4 py-2 rounded-2xl bg-sky-500"
+                    onClick={() => handleIncreaseProductQty(parseInt(id))}
+                >+</button>
+                <span className="mx-4">{getProductQty(parseInt(id))}</span>
+                <button
+                    className="px-4 py-2 rounded-2xl bg-sky-500"
+                    onClick={() => handleDecreaseProductQty(parseInt(id))}
+                >-</button>
+            </div>
+
+            <button className="bg-red-500 rounded px-6 py-2 mt-2">
+                حذف از سبد
+            </button>
+        </div>
     )
 }
