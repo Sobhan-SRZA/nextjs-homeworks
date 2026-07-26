ee276b487e7637671975ba73e493ac2c19455a12
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-07-26 03:48:49 +0330

    Update page.tsx

--- lesson5_onlineshop/app/store/page.tsx
+++ lesson5_onlineshop/app/store/page.tsx
@@ -1,10 +1,19 @@
 import Container from "@/components/Container";
-import ProductItem, { ProductItemProbs } from "@/components/ProductItem";
+import Pagination from "@/components/Pagination";
+import ProductItem, { ProductItemsPage } from "@/components/ProductItem";
 import Link from "next/link";
 
-export default async function Page() {
-  const result = await fetch("http://localhost:3001/products");
-  const products = await result.json() as ProductItemProbs[];
+interface StoreParams {
+  params: Promise<{}>;
+  searchParams: Promise<{ page: string; per_pages: string }>;
+}
+
+export default async function Store({ searchParams }: StoreParams) {
+  const page = (await searchParams).page ?? "1";
+  const per_pages = (await searchParams).per_pages ?? "5";
+
+  const result = await fetch(`http://localhost:3001/products?_page=${page}}&_per_page=${per_pages}`);
+  const products = await result.json() as ProductItemsPage;
 
   return (
     <Container>
@@ -12,7 +21,7 @@ export default async function Page() {
 
       <div className="grid grid-cols-4 gap-4">
         {
-          products.map(
+          products.data.map(
             (product) => (
               <Link
                 key={product.id}
@@ -26,6 +35,8 @@ export default async function Page() {
           )
         }
       </div>
+
+      <Pagination pageCount={products.pages} />
     </Container>
   )
 }
